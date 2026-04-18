71749342d01c0f93d1c40d0b15df237341f864c4
Mahi Pokharia <mahipokharia@gmail.com>
2026-04-18 22:30:59 +0530

    file  added

---
+++ src/main/java/com/project/scraper/WebScraperApplication.java
@@ -0,0 +1,13 @@
+package com.project.scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*Entry point for the Web Data Extraction System*/
+@SpringBootApplication
+public class WebScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebScraperApplication.class, args);
+    }
+}
